d8d8a2ce7f1039a50a3ed7e34b0b0486a08ba752
Saifullah Chowdhury <saifullahchy0177@gmail.com>
2021-04-21 22:11:42 +0600

    new quiz app added

---
+++ Quiz App/app.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        question: 'How old is the palent earth?',
+        a: '30b',
+        b: '40b',
+        c: '50b',
+        d: '60b',
+        ans: 'a'
+    },
+    {
+        question: 'What is the most use programming language?',
+        a: 'Java',
+        b: 'Python',
+        c: 'JavaScript',
+        d: 'C#',
+        ans: 'a'
+    },
+    {
+        question: 'Who is the Prime-minister of Bangladesh?',
+        a: 'Abul Kalam',
+        b: 'Obaidul Kader',
+        c: 'Sheikh Hasina',
+        d: 'Khaleda Zia',
+        ans: 'c'
+    },
+    {
+        question: 'What is const in javaScript?',
+        a: 'Function',
+        b: 'Variable',
+        c: 'String',
+        d: 'Input Type',
+        ans: 'd'
+    },
+    {
+        question: 'When JavaScript was lanuched?',
+        a: '2020',
+        b: '2021',
+        c: '2015',
+        d: 'none of the above',
+        ans: 'd'
+    },
+];
